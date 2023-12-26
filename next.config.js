1c07a4e37812ea8af9d28e146c2e6cc38bda63fa
Kunwar Padda <63855167+kunwarpadda@users.noreply.github.com>
2023-12-26 02:25:31 -0800

    made changes in next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	reactStrictMode: true,
+	experimental: {
+		fontLoaders: [
+			{ loader: "@next/font/google", options: { subsets: ["latin"] } },
+		],
+		images: true,
+	},
 	output: "export",
 };
 
