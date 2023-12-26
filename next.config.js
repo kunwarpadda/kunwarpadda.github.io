7f1a0791193559b479b6f4d1501825e9b5b35b32
Kunwar Padda <63855167+kunwarpadda@users.noreply.github.com>
2023-12-26 02:21:36 -0800

    added files

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	experimental: {
-		fontLoaders: [
-			{ loader: "@next/font/google", options: { subsets: ["latin"] } },
-		],
-	},
 	output: "export",
 };
 
